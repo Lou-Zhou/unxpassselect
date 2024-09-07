ff93623ac830d95f3b082684b6ff48d16b95fb62
Lou Zhou <86981747+Lou-Zhou@users.noreply.github.com>
2024-09-07 15:26:39 -0500

    Update train_soccermap_value.py

--- train_models/train_soccermap_value.py
+++ train_models/train_soccermap_value.py
@@ -13,52 +13,87 @@ import warnings
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 from unxpass.databases import SQLiteDatabase
 from unxpass.datasets_custom import PassesDataset, CompletedPassesDataset, FailedPassesDataset
-from unxpass.components import pass_selection, pass_value, pass_success
+from unxpass.components import pass_value
 pd.options.mode.chained_assignment = None
 
 DATA_DIR = Path("../stores/")
 db = SQLiteDatabase(DATA_DIR / "database.sql")
 dataset_success = partial(CompletedPassesDataset, DATA_DIR / "datasets" / "euro2020" / "train")
 dataset_fail = partial(FailedPassesDataset, DATA_DIR / "datasets" / "euro2020" / "train")
-value_fail = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel())
+STORES_FP = Path("../stores")
+
+value_fail_offensive = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel())
 print("training...")
-value_fail.train(dataset_fail, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
+#Offensive Fail
+value_fail_offensive.train(dataset_fail, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
 mlflow.set_experiment("pass_value/soccermap")
 with mlflow.start_run() as run:
     # Log the model
-    mlflow.pytorch.log_model(value_fail.model, "model")
+    mlflow.pytorch.log_model(value_fail_offensive.model, "model")
 
     # Retrieve the run ID
     run_id = run.info.run_id
-    fail = run_id
-    print(f"Fail Model saved with run_id: {run_id}")
+    fail_off = run_id
+    print(f"Fail Offensive Model saved with run_id: {run_id}")
 
-value_success = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel())
+#Defensive Fail
+value_fail_defensive = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel(), offensive = False, success = False)
 
-value_success.train(dataset_success, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
+value_fail_defensive.train(dataset_fail, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
+mlflow.set_experiment("pass_value/soccermap")
+with mlflow.start_run() as run:
+#     Log the model
+    mlflow.pytorch.log_model(value_fail_defensive.model, "model")
+
+        #Retrieve the run ID
+    run_id = run.info.run_id
+    fail_def = run_id
+    print(f"Fail Defensive Model saved with run_id: {run_id}")
+# Offensive Success
+value_success_offensive = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel(), offensive = True)
+
+value_success_offensive.train(dataset_success, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
 mlflow.set_experiment("pass_value/soccermap")
 with mlflow.start_run() as run:
     # Log the model
-    mlflow.pytorch.log_model(value_success.model, "model")
+    mlflow.pytorch.log_model(value_success_offensive.model, "model")
 
     # Retrieve the run ID
     run_id = run.info.run_id
-    success = run_id
-    print(f"Success Model saved with run_id: {run_id}")
+    success_off = run_id
+    print(f"Success Offensive Model saved with run_id: {run_id}")
+# Defensive Success
+value_success_defensive = pass_value.SoccerMapComponent(model = pass_value.PytorchSoccerMapModel(), offensive = False)
+
+value_success_defensive.train(dataset_success, trainer = {"accelerator": "cpu", "devices":1, "max_epochs": 10})
+mlflow.set_experiment("pass_value/soccermap")
+with mlflow.start_run() as run:
+    # Log the model
+    mlflow.pytorch.log_model(value_success_defensive.model, "model")
+
+    # Retrieve the run ID
+    run_id = run.info.run_id
+    success_def = run_id
+    print(f"Success Defensive Model saved with run_id: {run_id}")
 
 
 
 
+print(f"Fail Offensive Model saved as {fail_off}")
+print(f"Fail Defensive Model saved as {fail_def}")
+print(f"Success Offensive Model saved as {success_off}")
+print(f"Success Defensive Model saved as {success_def}")
 
-print(f"Fail Model saved as {fail}")
-print(f"Success Model saved as {success}")
-
-#success: f94362f83b6c4f2caa5da826daaacb8d
-#fail: f6f93a33c3ac4bc6a3b29442c0908dc7
+#success offensive: 5e7b6c613cd84f3e91702a73a36eebe6
+#success defensive: fc10352d1c4948b4ac556443e4de575a
+#fail offensive: 88a3c3e2ea6441c288d237c50ca542a0   
+#fail defensive:  28946631a0b54fe89dc83ca129815b4f 
 """
 Can then load with:
 model_pass_selection = pass_value.SoccerMapComponent(
     model=mlflow.pytorch.load_model(
         'run_id', map_location='cpu'
     )
-)"""
+)
+
+"""
